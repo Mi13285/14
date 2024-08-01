096995189bfb00bab7a903e0b7b7bda3822275d4
Mi13285 <mixkud95@inbox.ru>
2024-08-01 18:47:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+console.log(makeUpperCase("hello"));
+
+// function smash(words) {
+//   return words.join(" ");
+// }
+// console.log(smash(["hello"]));
